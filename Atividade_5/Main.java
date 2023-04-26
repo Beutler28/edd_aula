b8cb04899a463019c48e89d9dc115b6ce303550f
Mauricio Beutler <lambertibeutler@gmail.com>
2023-04-26 17:02:04 -0300

    Atividade 5

---
+++ Atividade_5/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        ListaEncadeada lista = new ListaEncadeada();
+        lista.addFinal(1);
+        lista.addFinal(2);
+        lista.addFinal(3);
+        
+    
+    }
+    
+}
