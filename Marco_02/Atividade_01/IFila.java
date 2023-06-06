950471cf274620f08cb8c58785d8279d5949c05e
Mauricio Beutler <lambertibeutler@gmail.com>
2023-06-06 13:45:33 -0300

    Adicionado tarefa 11 e 12

---
+++ Marco_02/Atividade_01/IFila.java
@@ -0,0 +1,6 @@
+public interface IFila{
+    public boolean add(Object info);
+    public boolean remove();
+    public boolean isEmpty();
+    public void imprimeFila();
+}
---
+++ Marco_02/Atividade_01/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args) {
+        FilaVet fila = new FilaVet(10);
+
+        fila.add(10);
+        fila.add(11);
+        fila.add(12);
+        fila.add(13);
+
+        fila.imprimeFila();
+    }
+}
